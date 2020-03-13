e3b96e47bcf819b016829924f33207e1ea73e12a
fabiomauro188 <fabiomauro188@gmail.com>
2020-03-13 20:56:29 -0300

    se agrego el ejer 4

---
+++ src/proyecto1/ejercicio4.java
@@ -0,0 +1,16 @@
+package proyecto1;
+
+import java.util.Scanner;
+
+public class ejercicio4 {
+	
+public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+	System.out.println("ingrese un numero  ");
+	
+	
+	
+}
+}
