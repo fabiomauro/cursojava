5740a4319b1a73092d116388da39f055a04ddbae
FABIO MAURO <62118152+fabiomauro@users.noreply.github.com>
2020-03-15 20:38:54 -0300

    Update ejercicio4.java
    se hicieron correcciones en el ejer 4

--- src/proyecto1/ejercicio4.java
+++ src/proyecto1/ejercicio4.java
@@ -10,6 +10,12 @@ public static void main(String[] args) {
 		
 	
 	System.out.println("ingrese  otro  numero  ");
+	System.out.println("ingrese  otro  numero  ");
+	
+	System.out.println("ingrese  otro  numero  ");
+	
+	
+	
 	
 	//  comentario 4 
 }
